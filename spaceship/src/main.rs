c151f9d8e9249722e8cac12a2f13f93cc98576a7
Viktor Marinho <56888067+viktormarinho@users.noreply.github.com>
2023-05-11 17:44:32 +0000

    traits example

--- spaceship/src/main.rs
+++ spaceship/src/main.rs
@@ -1,3 +1,101 @@
+use rand::{Rng, distributions::Alphanumeric};
+use std::io::Write;
+
+
+pub struct StoredData<T> {
+    pub id: String,
+    pub data: T
+}
+
+#[derive(Debug)]
+pub enum StoreErr {
+    UnknownError
+}
+
+#[derive(Debug)]
+pub enum LoadErr {
+    UnknownError
+}
+
+pub trait Storeable
+where Self: Sized {
+    fn store(&self) -> Result<StoredData<Self>, StoreErr>;
+}
+
+pub trait Loadable
+where Self: Sized {
+    fn load(id: String) -> Result<Option<Self>, LoadErr>;
+}
+
+struct Pessoa {
+    nome: String
+}
+
+impl Pessoa {
+    fn new(nome: String) -> Pessoa {
+        Pessoa { nome }
+    }
+}
+
+impl Storeable for Pessoa {
+    fn store(&self) -> Result<StoredData<Pessoa>, StoreErr> {
+        Err(StoreErr::UnknownError)
+    }
+}
+
+impl Storeable for String {
+    fn store(&self) -> Result<StoredData<Self>, StoreErr> {
+        let id: String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect(); 
+
+        let mut file = std::fs::OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open("database")
+            .unwrap();
+
+        if let Err(e) = writeln!(file, "{}", format!("{id}%%%{self}").to_string()) {
+            println!("Error occurred storing the string {self}");
+            dbg!(e);
+            return Err(StoreErr::UnknownError);
+        }
+
+        Ok(StoredData { id, data: self.to_string() })
+    }
+}
+
+impl Loadable for String {
+    fn load(id: String) -> Result<Option<Self>, LoadErr> {
+        let data = match std::fs::read_to_string("database") {
+            Err(e) => {
+                println!("Error occurred loading the string with id {id}");
+                return Err(LoadErr::UnknownError);
+            },
+            Ok(data) => data
+        }; 
+
+        data.split("\n").find(|row| {
+            id == row.split("%%%").nth(0).unwrap()
+        });
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let nome_legal = "Baltazar".to_string();
+
+    String::load("j02Knb5".to_string());
+
+    let registro = match nome_legal.store() {
+        Ok(data) => data,
+        Err(err) => {
+            println!("ACONTECEU UM ERRO:");
+            dbg!(err);
+            std::process::abort();
+        },
+    };
+
+    println!("Armazenei a string! o id é {}", registro.id);
 }
