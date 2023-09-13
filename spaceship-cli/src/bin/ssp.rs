935e25312b7f4442d7f430c77123001e805aa148
viktormarinho <viktormpcs@gmail.com>
2023-09-13 12:29:47 -0300

    Rust rover .idea config

--- spaceship-cli/src/bin/ssp.rs
+++ spaceship-cli/src/bin/ssp.rs
@@ -1,14 +1,17 @@
 use anyhow::Context;
 
 fn main() -> anyhow::Result<()> {
-    println!("spaceship");
+    let cwd = std::env::current_dir().unwrap();
+    println!("Spaceship CLI v0.0.1 - dir {}", cwd.display());
     do_thing()?;
 
     Ok(())
 }
 
 fn do_thing() -> anyhow::Result<()> {
-    let data = std::fs::read_to_string("../lib.rs").context("Could not read the lib.rs file")?;
+    let data = std::fs::read_to_string("./spaceship-cli/src/lib.rs")
+        .context("Could not read the lib.rs file")?;
+
 
     Ok(())
 }
